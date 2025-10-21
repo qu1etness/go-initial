a543ac1b4472336c12d703a5e48455443f016e13
Roman <kostiv.rr2005@gmail.com>
2025-10-21 17:02:01 +0300

    feat: add getInitiad function

--- main.go
+++ main.go
@@ -2,59 +2,22 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	infiniteCalculation()
+
+	initialName, initialSurname := getInitials("Roman Kostiv")
+	fmt.Println(initialName, initialSurname)
+
 }
 
-func infiniteCalculation() {
-	var enterValue uint8
-	for {
-		fmt.Println("---------Calculator--------")
-		fmt.Println("Enter the value:")
-		fmt.Println("1. Calculate new data")
-		fmt.Println("2. Exit")
-		fmt.Print("Enter value: ")
-		_, err := fmt.Scan(&enterValue)
+func getInitials(str string) (string, string) {
 
-		if err != nil {
-			fmt.Println("Error:", err)
-			var discard string
-			fmt.Scan(&discard)
-			fmt.Println("Invalid input, try again")
-			continue
-		}
+	splited := strings.Split(str, " ")
 
-		switch enterValue {
-		case 1:
-			userMass, userHeight := getInputValue()
-			index := calculateIndex(userMass, userHeight)
-			showInfo(userMass, userHeight, index)
-		case 2:
-			fmt.Println("Goodbye!")
-			return
-		default:
-			fmt.Println("Please enter a valid option (1 or 2).")
-		}
+	if len(splited) < 2 {
+		return string(splited[0][0]), "_"
 	}
+	return string(splited[0][0]), string(splited[1][0])
 }
-
-func getInputValue() (userMass float32, userHeight float32) {
-	fmt.Println("============Calculation============")
-	fmt.Println("Enter a mass (in kg):")
-	fmt.Scan(&userMass)
-	fmt.Println("Enter a height (in metres):")
-	fmt.Scan(&userHeight)
-	return
-}
-func calculateIndex(userMass float32, userHeight float32) float32 {
-	var index float32 = userMass / (userHeight * userHeight)
-	return index
-}
-func showInfo(userMass float32, userHeight float32, index float32) {
-	fmt.Printf("User with %.0f kg mass and %0.2f m height has %0.1f index \n", userMass, userHeight, index)
-}
-
-//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
