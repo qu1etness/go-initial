d10276f2e897513d8136961caf9f1aeed9aea70e
Roman <kostiv.rr2005@gmail.com>
2025-10-30 18:06:18 +0200

    feat: create fibbonacci function

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func fibonacci() func() int {
+	initial := 1
+
+	return func() int {
+		initial += initial
+		return initial
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
 }
