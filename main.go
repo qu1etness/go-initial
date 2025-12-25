a207875559b3135418b7fecd55f9f75722d61bc5
Roman Kostiv <kostiv.rr2005@gmail.com>
2025-12-25 21:53:04 +0200

    feat: add chenel representation

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	firstChanel := make(chan string)
+
+	go func() {
+		firstChanel <- "Concurrency in Go!"
+	}()
+
+	msg := <-firstChanel
+
+	fmt.Println(msg)
 }
