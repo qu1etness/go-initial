8dab3561b0aa9db251f893c4c7db092b8e5863e8
Roman Kostiv <kostiv.rr2005@gmail.com>
2025-12-25 21:38:42 +0200

    feat: initialize first goroutine

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func routine(num string) {
+	fmt.Println(num)
+}
+
 func main() {
 
+	go routine("1")
+	go routine("2")
+	go routine("3")
+
+	time.Sleep(time.Second * 2)
+
+	fmt.Println("Main function")
 }
