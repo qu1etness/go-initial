6c2895bdeeb713ecdf9cc752264a5a1fea549735
Roman Kostiv <kostiv.rr2005@gmail.com>
2026-01-05 13:23:53 +0200

    feat: implement another realisation

--- main.go
+++ main.go
@@ -9,15 +9,41 @@ func main() {
 	initialValues := []int{2, 4, 6, 7, 1}
 
 	//	Stage 1
-	dataChannel := sliceToChannel(initialValues)
+	dataChannel := sliceToChan2(initialValues)
 	//	Stage 2
-	finalChannel := sq(dataChannel)
+	finalChannel := sq2(dataChannel)
 	//  Stage 3
 
 	for value := range finalChannel {
 		fmt.Println(value)
 	}
 
+}
+
+func sq2(inputChan <-chan int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		for value := range inputChan {
+			out <- value * value
+		}
+	}()
+
+	return out
+}
+
+func sliceToChan2(initialValues []int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		for _, v := range initialValues {
+			out <- v
+		}
+	}()
+
+	return out
 }
 
 func sq(channel <-chan int) chan int {
